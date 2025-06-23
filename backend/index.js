26a8fefaab2cd4a0d7baaa3bc94acdbcb45d0039
Dhruv popat <dhruvpopat268@gmail.com>
2025-06-23 18:05:58 +0530

    checking

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(cors({
     "http://localhost:8081",
     "http://localhost:8080",
    "https://driver-go-admin-frontend.onrender.com",
-   "https://drive-go-frontend.onrender.com/"
+   "https://drive-go-frontend.onrender.com"
   ],
   credentials: true
 }));
